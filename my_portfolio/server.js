54d35988f4100ab21d8b3eb4487a490fa0588d8b
Priyanshi <priyanshi04@tradeindia.com>
2026-09-24 16:18:22 +0530

    visitor counter improved

--- my_portfolio/server.js
+++ my_portfolio/server.js
@@ -23,7 +23,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['https://priyanshi-eight.vercel.app/'],
   credentials: true,
 }))
 app.use(express.json())
